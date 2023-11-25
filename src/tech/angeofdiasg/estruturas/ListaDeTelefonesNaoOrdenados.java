5324f1b63eaaeea5e5a6961e52eff45c5f18151b
Allyson Santos <allyson.santos90@gmail.com>
2023-11-25 10:50:19 -0300

    Garantir Capacidade

--- src/tech/angeofdiasg/estruturas/ListaDeTelefonesNaoOrdenados.java
+++ src/tech/angeofdiasg/estruturas/ListaDeTelefonesNaoOrdenados.java
@@ -5,70 +5,92 @@ import java.util.Iterator;
 import tech.angeofdiasg.composicao.Telefone;
 
 public class ListaDeTelefonesNaoOrdenados {
-	//Vetor de telefones onde criaremos nossa lista
+	// Vetor de telefones onde criaremos nossa lista
 	private Telefone[] telefones;
-	//controle do tamanho
+	// controle do tamanho
 	private int tamanho;
-	//Capacidade inicial (pode ser alterada depois)
+	// Capacidade inicial (pode ser alterada depois)
 	private static final int CAPACIDADE_PADRAO = 10;
-	
-	//Construtor da lista
+
+	// Construtor da lista
 	public ListaDeTelefonesNaoOrdenados() {
 		this.telefones = new Telefone[CAPACIDADE_PADRAO];
 		this.tamanho = 0;
 	}
-	
-	//Verificador de vazio
+
+	// Verificador de vazio
 	public boolean isEmpty() {
 		return this.tamanho == 0;
 	}
-	
-	//Adicionar um telefone
+
+	private void garantirCapacidade() {
+		int novaCapacidade = telefones.length * 2;
+		Telefone[] listaTelefones = new Telefone[novaCapacidade];
+		for (int i = 1; i < tamanho; i++) {
+			listaTelefones[i] = telefones[i];
+		}
+	}
+
+	// Adicionar um telefone
 	public void addTelefone(Telefone telefone) {
-		//length ele pega a capacidade atual em tempo de execução,
+		//length ele pega a capacidade atual em tempo de execuÃ§Ã£o,
 		//Caso a CAPACIDADE_PADRAO seja alterada.
 		if (tamanho < telefones.length) {
-			//o tamanho já é o "index + 1", portando iremos usar o tamanho para adicionar, de pois incrementar o tamanho
+			//o tamanho jÃ¡ Ã© o "index + 1", portando iremos usar o tamanho para adicionar, de pois incrementar o tamanho
 			telefones[tamanho] = telefone;
 			//incremento do tamanho
 			this.tamanho = tamanho + 1;
 		}
+
+	// Adicionar um telefone VIDA LOKA
+	public void addTelefoneNoSQL(Telefone telefone) {
+		// length ele pega a capacidade atual em tempo de execuÃ§Ã£o,
+		// Caso a CAPACIDADE_PADRAO seja alterada.
+		if (tamanho == telefones.length) {
+			garantirCapacidade();
+			{
+				telefones[tamanho] = telefone;
+				this.tamanho = tamanho +1;
+			}
+		}
 	}
-	
-	//remover o telefone precisa buscar o index
+
+	// remover o telefone precisa buscar o index
 	public void removerTelefone(Telefone telefone) {
-		//Pegar o index do telefone desejado
-		int index  = buscarTelefoneIndex(telefone);
-		//validar se foi encontrado ou não, se foi encontrado o index será diferente de -1
-		if(index != -1) {
+		// Pegar o index do telefone desejado
+		int index = buscarTelefoneIndex(telefone);
+		// validar se foi encontrado ou nï¿½o, se foi encontrado o index serï¿½ diferente de
+		// -1
+		if (index != -1) {
 			for (int i = index; i < tamanho; i++) {
 				telefones[i] = telefones[i + 1];
+			}
+			// Pega o ï¿½ltimo elemento e coloca null nele, como tambï¿½m decrementa o tamanho
+			// (--tamanho ï¿½ o mesmo que tamanho -1)
+			telefones[--tamanho] = null;
 		}
-		//Pega o último elemento e coloca null nele, como também decrementa o tamanho (--tamanho é o mesmo que tamanho -1)
-		telefones[--tamanho] = null;
 	}
-	}
-	
-	//Procura o index do telefone
-	private int  buscarTelefoneIndex(Telefone telefone) {
-		for(int i = 0; i < tamanho; i++) {
-			//valida se os telefones são iguais
-			if(telefones[i].equals(telefone)) {
-				//retorna o index se encontrar
+
+	// Procura o index do telefone
+	private int buscarTelefoneIndex(Telefone telefone) {
+		for (int i = 0; i < tamanho; i++) {
+			// valida se os telefones sï¿½o iguais
+			if (telefones[i].equals(telefone)) {
+				// retorna o index se encontrar
 				return i;
 			}
 		}
-		//retorna -1 se não encontrar
-		System.out.println("Telefone não encontrado!");
+		// retorna -1 se nÃ£o encontrar
+		System.out.println("Telefone nÃ£o encontrado!");
 		return -1;
 	}
-	
-	//imprime os objetos, que nós colocamos o "toString" lá na classe Telefone
+
+	// imprime os objetos, que nï¿½s colocamos o "toString" lÃ¡ na classe Telefone
 	public void exibirTelefones() {
-		
-		//CRIE: a validação de vazio, se tiver vazio, nem entre no "for"
-		
-		for(int i = 0; i < tamanho; i++) {
+
+		// CRIE: a validaÃ§Ã£o de vazio, se tiver vazio, nem entre no "for"
+
+		for (int i = 0; i < tamanho; i++) {
 			System.out.println(telefones[i]);
 		}
 	}
